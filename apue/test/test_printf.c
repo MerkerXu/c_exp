f521b232a886455e7d807f97c59a395b843b834d
MerkerXu <MerkerXu@163.com>
2013-12-06 18:51:57 +0800

    add std buffered io & test var  args

---
+++ apue/test/test_printf.c
@@ -0,0 +1,48 @@
+#include <stdarg.h>
+
+void minprintf(char *fmt, ...)
+{/*{{{*/
+    va_list ap;     /* 依次指向每个无名参数 */
+    char *p, *sval;
+    int ival;
+    double dval;
+
+    va_start(ap, fmt);  /* 将ap指向第一个无名参数 */
+    for (p = fmt; *p; p++)
+    {/*{{{*/
+        if (*p != '%')
+        {/*{{{*/
+            putchar(*p);
+            continue;
+        }/*}}}*/
+
+        switch(*++p)
+        {/*{{{*/
+            case 'd':
+                ival = va_arg(ap, int);
+                printf("%d", ival);
+                break;
+            case 'f':
+                dval = va_arg(ap, double);
+                printf("%f",dval);
+                break;
+            case 's':
+                for(sval= va_arg(ap, char *); *sval; sval++)
+                    putchar(*sval);
+                break;
+            default:
+                putchar(*p);
+                break;
+        }/*}}}*/
+    }/*}}}*/
+
+    va_end(ap); /* 结束时的清理工作 */
+}/*}}}*/
+
+int
+main(void)
+{/*{{{*/
+    printf("%s:%d:%f\n", "test", 3, 0.75);
+    minprintf("%s:%d:%f\n", "test", 3, 0.75);
+    return 0;
+}/*}}}*/
